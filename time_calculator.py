63be42369c1a3d62899e9e61824d99cb39680f77
Nesmith-Charlesii <nesmith.charlesii@gmail.com>
2023-08-29 22:22:15 -0400

    working on catching the correct days apart

--- time_calculator.py
+++ time_calculator.py
@@ -58,10 +58,10 @@ def add_time(start_time, duration, day = None):
         calculated_time_obj["time_of_day"] = "PM"
     elif calculated_time_obj["hours"] in am_to_24hr_conversion or calculated_time_obj["hours"] == 24:
         calculated_time_obj["time_of_day"] = "AM"
-    
+
     calculated_time_obj["days_apart"] = (
-        duration_time_obj["hours"] // 12 if duration_time_obj["hours"] >= 24 else 
-        1 if duration_time_obj["hours"] // 12 == 1 else
+        round(duration_time_obj["hours"] / 24) if duration_time_obj["hours"] >= 24 else 
+        1 if round(duration_time_obj["hours"] / 24) == 1 else
         calculated_time_obj["days_apart"]
     )
 
@@ -81,19 +81,19 @@ def add_time(start_time, duration, day = None):
     if calculated_time_obj["hours"] > 12:
         calculated_time_obj["hours"] = calculated_time_obj["hours"] - 12
 
-    print(f'{calculated_time_obj["hours"]}:{calculated_time_obj["minutes"]:02d} {calculated_time_obj["time_of_day"]}{"," + " " + calculated_time_obj["day"].capitalize() if calculated_time_obj["day"] != None else " "} {"(" + str(calculated_time_obj["days_apart"]) + " days later)" if calculated_time_obj["days_apart"] > 1 else "next day" if calculated_time_obj["days_apart"] == 1 else ""}')
+    print(f'{calculated_time_obj["hours"]}:{calculated_time_obj["minutes"]:02d} {calculated_time_obj["time_of_day"]}{"," + " " + calculated_time_obj["day"].capitalize() if calculated_time_obj["day"] != None else " "} {"(" + str(calculated_time_obj["days_apart"]) + " days later)" if calculated_time_obj["days_apart"] > 1 else "(next day)" if calculated_time_obj["days_apart"] == 1 else ""}')
     
     
 #add_time("3:00 PM", "3:10")
 # Returns: 6:10 PM
 
-add_time("11:30 AM", "2:32", "Monday")
+#add_time("11:30 AM", "2:32", "Monday")
 # Returns: 2:02 PM, Monday
 
 #add_time("11:43 AM", "00:20")
 # Returns: 12:03 PM
 
-#add_time("10:10 PM", "3:30")
+add_time("10:10 PM", "3:30")
 # Returns: 1:40 AM (next day)
 
 add_time("11:43 PM", "24:20", "tueSday")
