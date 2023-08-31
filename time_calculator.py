c7ece92aff351d72c19c75b2459582a5fc8f5145
Nesmith-Charlesii <nesmith.charlesii@gmail.com>
2023-08-31 14:43:49 -0400

    All dates display correct data. Running tests

--- time_calculator.py
+++ time_calculator.py
@@ -30,7 +30,7 @@ def add_time(start, duration, day = None):
         "minutes": start_time_obj["minutes"] + duration_time_obj["minutes"],
         "time_of_day": start_time_obj["time_of_day"],
         "day": day,
-        "days_apart": None
+        "days_apart": 0
     }
     
     if calculated_time_obj["minutes"] >= 60:
@@ -47,58 +47,49 @@ def add_time(start, duration, day = None):
         "PM" if twenty_four_hour_format > 11 and calculated_time_obj["time_of_day"] == "AM" else 
         False
     )
-    #calculated_time_obj["hours"] = twenty_four_hour_format
-
     
     if calculated_time_obj["time_of_day"] == "PM" and calculated_time_obj["hours"] < 24:
         calculated_time_obj["days_apart"] = calculated_time_obj["days_apart"]
     else:
-        calculated_time_obj["days_apart"] = round(calculated_time_obj["hours"] / 24)
-        
-    print(f'{calculated_time_obj["hours"]}:{calculated_time_obj["minutes"]:02d} {calculated_time_obj["time_of_day"]} {calculated_time_obj["days_apart"] if calculated_time_obj["days_apart"] != None else ""}\n')
+        calculated_time_obj["days_apart"] += round(calculated_time_obj["hours"] / 24)
 
-    # days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+    calculated_time_obj["hours"] = twenty_four_hour_format
+
+    days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
     
-    # calculated_time_obj["days_apart"] = (
-    #     round(duration_time_obj["hours"] / 24) if duration_time_obj["hours"] > 24 else 
-    #     round(duration_time_obj["hours"] / 12) if duration_time_obj["hours"] < 24 else 
-    #     1 if round(duration_time_obj["hours"] / 24) == 1 else
-    #     calculated_time_obj["days_apart"]
-    # )
-    
-    # if day != None:
-    #     if calculated_time_obj["days_apart"] < 1:
-    #         calculated_time_obj["day"] = day.capitalize()
-    #     else:
-    #         day_index = days.index(day.lower())
-    #         calculated_day_index = day_index + calculated_time_obj["days_apart"]
-    #         calculated_time_obj["day"] = days[calculated_day_index % len(days)].capitalize()
+    if day != None:
+        if calculated_time_obj["days_apart"] < 1:
+            calculated_time_obj["day"] = day.capitalize()
+        else:
+            day_index = days.index(day.lower())
+            calculated_day_index = day_index + calculated_time_obj["days_apart"]
+            calculated_time_obj["day"] = days[calculated_day_index % len(days)].capitalize()
             
-    # if calculated_time_obj["hours"] > 12:
-    #     calculated_time_obj["hours"] = calculated_time_obj["hours"] - 12
+    if calculated_time_obj["hours"] > 12:
+        calculated_time_obj["hours"] = calculated_time_obj["hours"] - 12
 
-    # for i in calculated_time_obj:
-    #     if type(calculated_time_obj[i]) == int:
-    #         calculated_time_obj[i] = str(calculated_time_obj[i])
-    #         if i == "minutes" and len(calculated_time_obj[i]) < 2:
-    #             calculated_time_obj[i] = calculated_time_obj[i].zfill(2)
+    for i in calculated_time_obj:
+        if type(calculated_time_obj[i]) == int:
+            calculated_time_obj[i] = str(calculated_time_obj[i])
+            if i == "minutes" and len(calculated_time_obj[i]) < 2:
+                calculated_time_obj[i] = calculated_time_obj[i].zfill(2)
 
-    # time_format = f'{calculated_time_obj["hours"]}:{calculated_time_obj["minutes"]} {calculated_time_obj["time_of_day"]}'
+    time_format = f'{calculated_time_obj["hours"]}:{calculated_time_obj["minutes"]} {calculated_time_obj["time_of_day"]}'
     
-    # time_day_format = f'{calculated_time_obj["hours"]}:{calculated_time_obj["minutes"]} {calculated_time_obj["time_of_day"]}, {calculated_time_obj["day"]}'
+    time_day_format = f'{calculated_time_obj["hours"]}:{calculated_time_obj["minutes"]} {calculated_time_obj["time_of_day"]}, {calculated_time_obj["day"]}'
 
-    # time_days_apart_format = f'{calculated_time_obj["hours"]}:{calculated_time_obj["minutes"]} {calculated_time_obj["time_of_day"]} {"(" + calculated_time_obj["days_apart"] + " days apart" + ")" if int(calculated_time_obj["days_apart"]) > 1 else "(next day)" if int(calculated_time_obj["days_apart"]) == 1 or (int(calculated_time_obj["days_apart"]) <= 1 and calculated_time_obj["time_of_day"] == "AM") and int(calculated_time_obj["days_apart"]) != 0 else ""}'
+    time_days_apart_format = f'{calculated_time_obj["hours"]}:{calculated_time_obj["minutes"]} {calculated_time_obj["time_of_day"]} {"(" + calculated_time_obj["days_apart"] + " days apart" + ")" if int(calculated_time_obj["days_apart"]) > 1 else "(next day)" if int(calculated_time_obj["days_apart"]) == 1 or (int(calculated_time_obj["days_apart"]) <= 1 and calculated_time_obj["time_of_day"] == "AM") and int(calculated_time_obj["days_apart"]) != 0 else ""}'
 
-    # full_time_format = f'{calculated_time_obj["hours"]}:{calculated_time_obj["minutes"]} {calculated_time_obj["time_of_day"]}, {calculated_time_obj["day"]} {"(" + calculated_time_obj["days_apart"] + " days apart" + ")" if int(calculated_time_obj["days_apart"]) > 1 else "(next day)" if int(calculated_time_obj["days_apart"]) == 1 or (int(calculated_time_obj["days_apart"]) <= 1 and calculated_time_obj["time_of_day"] == "AM") and int(calculated_time_obj["days_apart"]) != 0 else ""}'
+    full_time_format = f'{calculated_time_obj["hours"]}:{calculated_time_obj["minutes"]} {calculated_time_obj["time_of_day"]}, {calculated_time_obj["day"]} {"(" + calculated_time_obj["days_apart"] + " days apart" + ")" if int(calculated_time_obj["days_apart"]) > 1 else "(next day)" if int(calculated_time_obj["days_apart"]) == 1 or (int(calculated_time_obj["days_apart"]) <= 1 and calculated_time_obj["time_of_day"] == "AM") and int(calculated_time_obj["days_apart"]) != 0 else ""}'
 
-    # new_time = (
-    #     full_time_format.strip() if calculated_time_obj["day"] != None and calculated_time_obj["days_apart"] != None else
-    #     time_day_format.strip() if calculated_time_obj["day"] != None and calculated_time_obj["days_apart"] == None else
-    #     time_days_apart_format.strip() if calculated_time_obj["days_apart"] != None and calculated_time_obj["day"] == None else
-    #     time_format.strip()
-    # )
+    new_time = (
+        full_time_format.strip() if calculated_time_obj["day"] != None and calculated_time_obj["days_apart"] != 0 else
+        time_day_format.strip() if calculated_time_obj["day"] != None and calculated_time_obj["days_apart"] == 0 else
+        time_days_apart_format.strip() if calculated_time_obj["days_apart"] != 0 and calculated_time_obj["day"] == None else
+        time_format.strip()
+    )
 
-    # print(new_time, "\n")
+    print(new_time)
     
     
 add_time("3:00 PM", "3:10")
