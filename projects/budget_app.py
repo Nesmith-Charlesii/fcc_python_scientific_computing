bf1e2858fb068a0543f40d80c65545558bf58a58
Nesmith-Charlesii <nesmith.charlesii@gmail.com>
2023-09-01 15:54:42 -0400

    testing transfer method

--- projects/budget_app.py
+++ projects/budget_app.py
@@ -31,8 +31,22 @@ class Category:
     def transfer(self, amount, budget_category):
         balance = self.get_balance()
         if balance > amount:
+
             self.ledger.append({"amount": -amount, "description": f'Transfer to {budget_category.category}'})
+            print(f'Transfer to {budget_category.category}')
+
             budget_category.deposit(amount, f'Transfer from {self.category}')
+            print(f'Transfer from {self.category}')
+
             return True
         return False
         
+entertainment = Category("entertainment")
+entertainment.deposit(500, "entertainment system")
+
+clothing = Category("clothing")
+clothing.deposit(300, "casual wear")
+
+entertainment.transfer(200, clothing)
+entertainment.get_balance()
+clothing.get_balance()
