ca1433603d9f2bed9577bdb8e6d19d132e605968
Nesmith-Charlesii <nesmith.charlesii@gmail.com>
2023-09-03 14:38:49 -0400

    working on recursive way to sum and percent category expenses

--- projects/budget_app.py
+++ projects/budget_app.py
@@ -84,6 +84,33 @@ class Category:
             # ACCOUNT FOR TRANSACTION GREATER THAN 7 DIGITS
 
 
+def create_spend_chart(categories):
+
+    chart_title = "Percentage spent by category"
+    print(categories)
+    category_index = (len(categories) - 1)
+    expense_percentage = {}
+    
+    def percent_spent(category_index):
+        if category_index == 0:
+            return "recursion complete"
+        else:
+            pass
+            # print(categories[category_index])
+            # category_name = categories[category_index].category
+            # expense_sum = 0
+
+            # for i in categories[category_index]:
+            #     print(i)
+                # if i["amount"] < 0:
+                #     print(i["amount"])
+                #     expense_sum += i["amount"]
+            # print(f'{chart_title}\n{expense_sum}')
+            # return percent_spent(category_index - 1)
+        
+    # percent_spent(category_index)
+
+    
 food = Category("Food")
 clothing = Category("Clothing")
 
@@ -91,5 +118,5 @@ food.deposit(1000, "initial deposit")
 food.withdraw(10.15, "groceries")
 food.withdraw(15.898434, "restaurant and more food")
 food.transfer(50, clothing)
-
-food.display_budget()
+#food.display_budget()
+create_spend_chart([food, clothing])
