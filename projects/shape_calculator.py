9c3f8723b5cc0dda691c04317f4ba4095201c4ca
Nesmith-Charlesii <nesmith.charlesii@gmail.com>
2023-09-11 12:33:31 -0400

    methods of Rectangle class setup

--- projects/shape_calculator.py
+++ projects/shape_calculator.py
@@ -1,4 +1,36 @@
 class Rectangle:
-    pass
-    class Square:
+    def __init__(self, width, height) -> None:
+        self.width = width
+        self.height = height
+    
+    def set_width(self):
         pass
+
+    def set_height(self):
+        pass
+
+    def get_area(self):
+        area = self.width * self.height
+        return area
+
+    def get_perimeter(self):
+        perimeter = (2 * self.width) + (2 * self.height)
+        return perimeter
+
+    def get_diagonal(self):
+        # '**' is an operator used for exponentiation 
+        diagonal = ((self.width ** 2) + (self.height ** 2)) ** .5
+        return diagonal
+
+    def get_picture(self):
+        if self.height > 50 or self.width > 50:
+            return "Too big for picture."
+        
+        lines = self.height
+        plus_signs = self.width
+    
+    def get_amount_inside(self, shape):
+        pass
+
+class Square:
+    pass
