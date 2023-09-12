a1d8fd9a35f79a663c811935754ca6b40a99542f
Nesmith-Charlesii <nesmith.charlesii@gmail.com>
2023-09-12 09:40:33 -0400

    setting up the experiment function

--- projects/probability_calculator.py
+++ projects/probability_calculator.py
@@ -14,15 +14,36 @@ class Hat:
         print(f'Inital ball count: {self.contents}\n')
 
     def draw(self, draw_count):
-        drawn_balls = []
+        if draw_count > len(self.contents):
+            return self.contents
+        
+        balls_drawn = []
         for i in range(0, draw_count):
             choice = self.contents.pop(random.randrange(0, len(self.contents)))
-            drawn_balls.append(choice)
-        print(f'Balls drawn: {drawn_balls}')
+            balls_drawn.append(choice)
+        print(f'Balls drawn: {balls_drawn}')
         print(f'Balls left: {self.contents}')
+        return balls_drawn
+
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    pass
+    hat_copy = copy.copy(hat)
+    expected_ball_list = []
 
-hat = Hat(yellow=3, blue=2, green=6)
-hat.draw(4)
+    # First create a list of expected balls from expected_balls dict
+    for k,v in expected_balls.items():
+        for i in range(0, v):
+            expected_ball_list.append(k)
+
+    balls_drawn = hat_copy.draw(5)
+
+
+
+hat1 = Hat(yellow=3, blue=2, green=6)
+#hat1.draw(4)
+experiment(
+    hat=hat1,
+    expected_balls={"red": 2, "green": 1},
+    num_balls_drawn=5,
+    num_experiments=2000
+)
