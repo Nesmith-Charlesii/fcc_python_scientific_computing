212cae4d2a4e37a970e957271c86ae952f125cee
Nesmith-Charlesii <nesmith.charlesii@gmail.com>
2023-09-12 10:35:00 -0400

    outcome matches by way of dict. logic to finding matches works

--- projects/probability_calculator.py
+++ projects/probability_calculator.py
@@ -11,36 +11,39 @@ class Hat:
         for k,v in kwargs.items():
             for i in range(0, v):
                 self.contents.append(k)
-        print(f'Inital ball count: {self.contents}')
+
+    def __str__(self) -> str:
+        return f'{self.contents}'
 
     def draw(self, draw_count):
         if draw_count > len(self.contents):
+            print(f'draw count exceeds amount of balls in hat ***** {self.contents}')
             return self.contents
         
         balls_drawn = []
         for i in range(0, draw_count):
             choice = self.contents.pop(random.randrange(0, len(self.contents)))
             balls_drawn.append(choice)
-        print(f'Balls drawn: {balls_drawn}')
-        print(f'Balls left: {self.contents}')
+
         return balls_drawn
 
 
+ball_outcome = {}
+
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     if num_experiments < 1:
-        print("COMPLETE")
+        if ball_outcome == expected_balls:
+            print(f'ball_outcome: {ball_outcome}\n** matches **\nexpected_balls: {expected_balls}')
+        else: 
+            print(f'expected outcome does not match outcome')
     else:
-        hat_copy = copy.copy(hat)
-        expected_ball_list = []
-
-        # First create a list of expected balls from expected_balls dict
-        for k,v in expected_balls.items():
-            for i in range(0, v):
-                expected_ball_list.append(k)
-
+        # Using copy.deepcopy as to not affect the original instance
+        hat_copy = copy.deepcopy(hat)
         balls_drawn = hat_copy.draw(num_balls_drawn)
-        print(f'expected_balls: {expected_ball_list}')
-        print(f'balls_drawn: {balls_drawn}')
+
+        for k,v in expected_balls.items():
+            if balls_drawn.count(k) == v:
+                ball_outcome [k] = v
         
         experiment(hat, expected_balls, num_balls_drawn, num_experiments=num_experiments-1)
 
