72365960d69fb3c0e32c093ae4baaade805961b8
Nesmith-Charlesii <nesmith.charlesii@gmail.com>
2023-09-13 18:44:46 -0400

    project passes replit tests

--- projects/probability_calculator.py
+++ projects/probability_calculator.py
@@ -31,20 +31,23 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     match_count = 0
     balls_found = {}
     print(f'Expected balls ---> {expected_balls}\n')
-    for i in range(num_experiments):
+    for i in range(0, num_experiments):
         hat_copy = copy.deepcopy(hat)
         balls_drawn = hat_copy.draw(num_balls_drawn)
         for k,v in expected_balls.items():
             if balls_drawn.count(k) >= v:
                 balls_found[k] = v
-                print(f'Balls found: {balls_found}')
-                match_count += 1 if balls_found == expected_balls else 0
+            else: 
+                balls_found[k] = balls_drawn.count(k)
+        print(f'Balls found: {balls_found}')
+        match_count += 1 if balls_found == expected_balls else 0
+        
 
-    print(f'match_count: {match_count}/{num_experiments}\nprobability: {match_count/num_experiments}')
+    print(f'\nmatch_count: {match_count}/{num_experiments}\nprobability: {match_count/num_experiments}')
     return match_count/num_experiments
 
 hat1 = Hat(blue=3,red=2,green=6)
-#hat1.draw(4)
+
 experiment(
     hat=hat1,
     expected_balls={"blue":2,"green":1},
